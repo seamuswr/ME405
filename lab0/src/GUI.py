dc96e6f7444a13d1c1050bcee7352c3f954456da
seamuswr <108034107+seamuswr@users.noreply.github.com>
2024-01-23 13:23:04 -0800

    Added data reading and plotting

--- lab0/src/GUI.py
+++ lab0/src/GUI.py
@@ -3,14 +3,34 @@ import tkinter
 from random import random
 from serial import Serial
 from matplotlib.figure import Figure
+from matplotlib import pyplot
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import time
+
+time_data = [range(1)]
+height_data = [range(1)]
 
 if __name__ == "__main__":
     ser = Serial('/dev/tty.usbmodem204F377739472', timeout=10)
     ser.write(b'\x02')
     ser.write(b'\x04')
-    print(ser.readlines())
+    line = ser.readline().decode('utf-8').rstrip().split(",", 1)
+    while line[0] != "END":
+        try:
+            float(line[0])
+            float(line[1])
+        except (ValueError, IndexError):
+            print("exception")
+        else:
+            time_data.append(float(line[0]))
+            height_data.append(float(line[1]))
+        finally:
+            line = ser.readline().decode('utf-8').rstrip().split(",", 1)
+
+    pyplot.plot(time_data[1:], height_data[1:])
+    #pyplot.xlabel(time_data[0])
+    #pyplot.ylabel(height_data[0])
+    pyplot.show()
 
 """! TO DO:
 1. Modify homework 0 to take in serial data and end at 'End'
--- lab0/src/step_response.py
+++ lab0/src/step_response.py
@@ -17,11 +17,12 @@ def timer_int(dummy):
 def step_response():
     """! Doxygenstyle dostring for step response function
 """
-    pinC0.value(1)
     timmy.callback(timer_int)
+    pinC0.value(1)
     while not volt_queue.full():
         pass
     timmy.deinit()
+    pinC0.value(0)
 
 
 if __name__ == "__main__":
@@ -33,10 +34,9 @@ if __name__ == "__main__":
     QUEUE_SIZE = 200
     volt_queue = cqueue.IntQueue(QUEUE_SIZE)
     pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
-    pinC0.value(0) 
-    time.sleep(2)
     timmy = pyb.Timer(1, freq=100)
     step_response()
+
     for idx in range(QUEUE_SIZE):
         print(f'{10*idx}, {volt_queue.get()*3.3/4096}')
     print('END')
